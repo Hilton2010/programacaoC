5ddf57e4f724c1d82c8d74286c6ee75dcfc934d7
Hilton2010 <hiltonsilvaneto@outlook.com>
2025-08-16 17:23:57 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+  int pop1, tur1, pop2, tur2;
+  char estado1, codigo_carta1, cidade1, estado2, codigo_carta2;
+  float area1, pib1, area2, pib2;
+
+  printf("Digite o estado da Carta 1: \n");
+  scanf("%c", &estado1);
